e0ed2af316e6f98901700689be1a47540fd6c559
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2024-04-12 19:22:00 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,15 @@
-function appendToDisplay(value) {
-    document.getElementById('display').value += value;
-}
+function addToDisplay(value) {
+      document.getElementById('display').value += value;
+    }
 
-function clearDisplay() {
-    document.getElementById('display').value = '';
-}
+    function calculate() {
+      try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+      } catch(error) {
+        document.getElementById('display').value = 'Error';
+      }
+    }
 
-function calculate() {
-    let expression = document.getElementById('display').value;
-    let result = eval(expression);
-    document.getElementById('display').value = result;
-}
+    function clearDisplay() {
+      document.getElementById('display').value = '';
+    }
